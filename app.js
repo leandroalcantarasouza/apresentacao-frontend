a632b4c3096f60811ca1a1d4b5199260bf79984d
Leandro Souza <leandro.alcantara.souza@gmail.com>
2019-02-12 21:48:59 -0200

    teste

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+const proxy = require('http-proxy-middleware');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+var apiProxy = proxy('/api/*', {target: 'http://172.30.197.118:8080/'});
+app.use(apiProxy);
+
+
+module.exports = app;
