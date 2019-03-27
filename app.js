80569f4e6817afe268712b7fe56faa5cc3f610ec
Leandro Souza <leandro.alcantara.souza@gmail.com>
2019-03-26 22:48:32 -0300

    testing variable

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 console.log(`api = http://${process.env.BACKEND_ADDRESS}:8080/`);
-var apiProxy = proxy('/api/*', {target: `http://${process.env.BACKEND_ADDRESS}:8080/`});
+var apiProxy = proxy('/api/*', {target: `http://${process.env.BACKEND_ADDRESS}/`});
 app.use(apiProxy);
 
 
